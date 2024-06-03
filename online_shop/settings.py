885dca543d82c8fdff68c50f1c98fdd34a9607d2
Маріна Булах <marinabulah@MacBook-Pro-Marina.local>
2024-06-03 11:04:45 +0300

    add new fix

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -128,8 +128,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "online_shop/static")
 ]
+
 
 
 # directory that we want to store uploaded files
