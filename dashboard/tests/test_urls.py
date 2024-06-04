8507cb44ec45203d9776f042d6b0721828846e0f
VolodymyrKhodatskyi <datomir777@gmail.com>
2024-06-05 01:29:44 +0300

    Prepearing for heroku

--- dashboard/tests/test_urls.py
+++ dashboard/tests/test_urls.py
@@ -6,14 +6,14 @@ class TestUrls(SimpleTestCase):
     def test_add_category_url(self):
         url = reverse('dashboard:add_category')
         print(url)
-        self.assertEquals(resolve(url).func, add_category)
+        self.assertEqual(resolve(url).func, add_category)
 
     def test_add_product_url(self):
         url = reverse('dashboard:add_product')
         print(url)
-        self.assertEquals(resolve(url).func, add_product)
+        self.assertEqual(resolve(url).func, add_product)
 
     def test_orders_url(self):
         url = reverse('dashboard:orders')
         print(url)
-        self.assertEquals(resolve(url).func, orders)
+        self.assertEqual(resolve(url).func, orders)
