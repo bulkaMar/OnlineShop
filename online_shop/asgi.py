56dbaf9953435f9a558d09598b478138e2b1cfee
VolodymyrKhodatskyi <datomir777@gmail.com>
2024-06-05 00:54:25 +0300

    Prepearing for heroku

--- online_shop/asgi.py
+++ online_shop/asgi.py
@@ -4,7 +4,5 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
 
-import django
-django.setup()
 
 application = get_asgi_application()
--- online_shop/wsgi.py
+++ online_shop/wsgi.py
@@ -6,10 +6,5 @@ from accounts.views import create_manager
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
 
-import django
-django.setup()
-
-#creating user with 'manager' role
-create_manager()
 
 application = get_wsgi_application()
