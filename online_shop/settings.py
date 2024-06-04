3eebe7625a82edf06a6e3b1a1064eb2e3268b4d7
VolodymyrKhodatskyi <datomir777@gmail.com>
2024-06-05 00:42:36 +0300

    Prepearing for heroku

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -15,7 +15,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOST = os.environ.get('ALLOWED_HOST', '*')
-DJANGO_SETTINGS_MODULE = os.environ.get('DJANGO_SETTINGS_MODULE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
